1be48b2521f4e055b43dc117a3c4ce32e4959b19
clifford <cliffoubre@gmail.com>
2022-05-09 14:22:15 +0100

    start-creation-of-homepage

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <header>
+        <a href="/">crazy market</a>
+      </header>  
+        <main>
+          List products 
+        </main>
+    </div>
+   
+  );
+}
+
+
+export default App;
