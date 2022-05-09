70a9a8b24c30896c1c9ff993eef8b6c98b5610b2
clifford <cliffoubre@gmail.com>
2022-05-09 21:21:02 +0100

    second-commit

---
+++ back/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirt',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInStoke: 10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality products',
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirt',
+            image:'/images/p2.jpg',
+            price: 250,
+            countInStoke: 20,
+            brand:'Adidas',
+            rating:4.0,
+            numReviews:10,
+            description:'high quality products',
+        },
+        {
+            name: 'Nike slim pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image:'/images/p3.jpg',
+            price: 25,
+            countInStoke: 15,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:14,
+            description:'high quality products',
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants',
+            image:'/images/p4.jpg',
+            price: 65,
+            countInStoke: 5,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality products',
+        }
+    ]
+}
+export default data; 
